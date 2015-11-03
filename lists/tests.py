f91b2cf92dca532c77d1ce573873da5f55a8f7d7
Hongmei Qiu <hongmeiq@usc.edu>
2015-11-03 15:53:26 -0800

    Chapter 5

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -14,6 +15,15 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        # question for myself: what happen if there's variables in the template?
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)        
+    
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html  = render_to_string('home.html', {'new_item_text': 'A new list item'})
+        self.assertEqual(response.content.decode(), expected_html)
