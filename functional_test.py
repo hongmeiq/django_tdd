5bd45dcc9e15eac3f27af0a28f8948424110daab
Hongmei Qiu <hongmeiq@usc.edu>
2015-11-03 15:21:34 -0800

    Front page HTML now generated from a template

--- functional_test.py
+++ functional_test.py
@@ -9,7 +9,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.implicitly_wait(3)
         
     def tearDown(self):
-        self.browser.quite()
+        self.browser.quit()
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
@@ -30,7 +30,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: buy peacock feathers' for row in rows)
+            any(row.text == '1: buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )        
 
         self.fail('Finish the test!')
