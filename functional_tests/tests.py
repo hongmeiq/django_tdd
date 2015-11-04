4e53d2d1b0f7a16e80fca9e9bef01fcca7e078f1
Hongmei Qiu <hongmeiq@usc.edu>
2015-11-04 10:35:50 -0800

    Chapter 6: adding functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -32,6 +32,8 @@ class NewVisitorTest(LiveServerTestCase):
         
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')        
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 #         import time
 #         time.sleep(10)
@@ -43,6 +45,27 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
+        # now a new user, Francis, comes along to the site
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        
+        self.browser.get(self.live_server_url)
+        # Francis visits the home page.  There is no sign of Edit's List
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(keys.ENTER)
+        
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edit_list_url)
+        
+        page_text = self.browser.find_element_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
         self.fail('Finish the test!')
         
