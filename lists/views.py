2deda10568548c5183dbb929c378829ad4e00d7d
Hongmei Qiu <hongmeiq@usc.edu>
2015-11-05 16:37:01 -0800

    Use get_absolute_url on List model to DRY urls in views

--- lists/views.py
+++ lists/views.py
@@ -1,19 +1,14 @@
-from django.http import HttpResponse
+from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 from lists.models import Item, List
-from django.core.exceptions import ValidationError
-# Create your views here.
+
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
-    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
         item.save()
@@ -21,10 +16,22 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
-    return redirect('/lists/%d/' % (list_.id,))
+    return redirect(list_)
 
-def add_item(request, list_id):
+
+def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)    
-    return redirect('/lists/%d/' % (list_.id,))
+    error = None
+
+    if request.method == 'POST':
+        try:
+
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect(list_)
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, 'list.html', {'list': list_, 'error': error})
 
