d2486ca81eab47fdf3f00f66a479b10d08430c78
Hongmei Qiu <hongmeiq@usc.edu>
2015-11-09 11:55:17 -0800

    chapter 11 finished

--- lists/forms.py
+++ lists/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
@@ -16,4 +17,10 @@ class ItemForm(forms.models.ModelForm):
         }
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
-        }        
+        }
+
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
+
